053ce71e68ef79933256e696c0794c55cc2fbbfe
Oscar-Obando <obando1524@gmail.com>
2024-03-11 21:38:07 -0500

    añadiendo diseño y clientes

---
+++ src/app/components/add-edit-cliente/add-edit-cliente.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddEditClienteComponent } from './add-edit-cliente.component';
+
+describe('AddEditClienteComponent', () => {
+  let component: AddEditClienteComponent;
+  let fixture: ComponentFixture<AddEditClienteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddEditClienteComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AddEditClienteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/listclientes/listclientes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listclientes',
+  standalone: true,
+  imports: [],
+  templateUrl: './listclientes.component.html',
+  styleUrl: './listclientes.component.css'
+})
+export class ListclientesComponent {
+
+}
